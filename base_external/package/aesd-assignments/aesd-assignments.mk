1c4f42ac8bcbd783475d5bf7857b2307391d924d
thra6614 <thra6614@Ubuntu.myguest.virtualbox.org>
2024-04-02 19:21:16 -0600

    change buildroot for assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 17391875febda72f27c6143b332c38662bb121bf
+AESD_ASSIGNMENTS_VERSION = 4da4116f83e92d56a9aad1a6235cd4e77b18d1a0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-AESDCHAR_VERSION = '68bae9fd71310c5b31dcac74b8861b3fee8ae962'
+AESDCHAR_VERSION = '4da4116f83e92d56a9aad1a6235cd4e77b18d1a0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
